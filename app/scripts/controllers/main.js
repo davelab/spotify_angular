0ff34fe467ae211acd98eaf6d3574edc67fe79e5
Davide <davidecariani7@gmail.com>
2014-09-23 23:24:38 +0200

    cleaning controller code and uniquefy ng-repeat in view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,16 +8,11 @@
  * Controller of the spotifyAngularApp
  */
 angular.module('spotifyAngularApp')
-  .controller('MainCtrl', function ($scope, spotifyService, _) {
+  .controller('MainCtrl', function ($scope, spotifyService) {
     var options = {
       'limit': 10
     };
     spotifyService.search('mannarino', 'album', options).then(function(data) {
-        var albums = ['bar della rabbia'];
-        console.log(albums);
-        console.log('::::::::::::::::::::::::::::::::::::::::::::::::::::::::');
-        console.log(_.uniq(albums.name));
-
-        //$scope.albums = _.uniq(data.albums.items);
+        $scope.albums = data.albums.items;
     });
   });
